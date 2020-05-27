3c12bfce8cd9fc8237827dfb74fa2b456915cfe6
William Downey-Schell <Will@Williams-MacBook-Air.local>
2020-05-27 14:42:54 -0700

    styled page and h1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
-import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons'
-import { robots } from './robots'
+import App from './App'
 
 ReactDOM.render(
   <div>
-    <CardList robots={robots}/>
+    <App />
   </div>,
   document.getElementById('root')
 );
